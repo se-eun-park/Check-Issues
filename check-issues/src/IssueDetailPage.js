13319cf774631f454c860011f3fbcc65a0bccd6a
박세은 <seeunbag30@gmail.com>
2023-11-03 17:09:03 +0900

    Update code : IssueDetailPage 정렬을 위한 html 코드 추가

--- check-issues/src/IssueDetailPage.js
+++ check-issues/src/IssueDetailPage.js
@@ -23,15 +23,19 @@ const IssueDetailPage = () => {
   if (!issueDetail) return <div>Loading...</div>;
 
   return (
-    <div>
-    <header>
+    <div className='detail-background'>
+      <span></span>
+    <div className='display-total'>
       <h1>facebook / react</h1>
-    </header>
-    
-      <h2>{issueDetail.title}</h2>
-      <p>Written by {issueDetail.user.login} on {new Date(issueDetail.created_at).toLocaleDateString()}</p>
-      <img src={issueDetail.user.avatar_url} alt={`${issueDetail.user.login}'s avatar`} />
-      <p>{issueDetail.body}</p>
+      <div className='dispay-issue'>
+        <section className='detail-contents'>
+          <h2>{issueDetail.title}</h2>
+          <img className='creator-img' src={issueDetail.user.avatar_url} alt={`${issueDetail.user.login}'s avatar`} />
+          <p className='creator-ifm'>Written by {issueDetail.user.login} on {new Date(issueDetail.created_at).toLocaleDateString()}</p>
+          </section>
+          <p className='creator-issue'>{issueDetail.body}</p>
+      </div>
+    </div>
     </div>
   );
 };
