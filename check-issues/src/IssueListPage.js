e1642ecd962a4ce11ada01001ef5843565e5213a
박세은 <seeunbag30@gmail.com>
2023-11-03 17:08:29 +0900

    Fixcode

--- check-issues/src/IssueListPage.js
+++ check-issues/src/IssueListPage.js
@@ -57,58 +57,62 @@ const IssueListPage = () => {
       <BackgroundSection />
       <MainSection />
       <ContentsSection />
-    <header>
-      <h1>facebook / react</h1>
-    </header>
-
-    <table>
-      <thead>
-        <tr>
-          <th>이슈번호</th>
-          <th>이슈제목</th>
-          <th>작성자</th>
-          <th>작성일</th>
-          <th>코멘트수</th>
-        </tr>
-      </thead>
-      <tbody>
-      {issue.map((item, index) => {
-          if (issue.length === index + 1) {
-            return (
-              <tr ref={lastIssueElementRef} key={item.id}>
-                <td>{item.number}</td>
-                <td><Link to={`/issuelist/${item.number}`}>{item.title}</Link></td>
-                <td>{item.user.login}</td>
-                <td>{new Date(item.created_at).toLocaleDateString()}</td>
-                <td>{item.comments}</td>
-              </tr>
-              );
-          } else {
-            return (
-              <React.Fragment key={item.id}>
-                <tr>
-                <td>{item.number}</td>
-                <td><Link to={`/issuelist/${item.number}`}>{item.title}</Link></td>
-                <td>{item.user.login}</td>
-                <td>{new Date(item.created_at).toLocaleDateString()}</td>
-                <td>{item.comments}</td>
+    <div className="table-container">
+      <section className="contents">
+      <h2>facebook / react</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Issue Title</th>
+            <th>Creator</th>
+            <th>Date</th>
+            <th>Comments</th>
+            <th className="thead-empty"></th>
+          </tr>
+        </thead>
+        <tbody>
+        {issue.map((item, index) => {
+            if (issue.length === index + 1) {
+              return (
+                <tr ref={lastIssueElementRef} key={item.id}>
+                  <td>{item.number}</td>
+                  <td><a href={`/issuelist/${item.number}`} target="_blank" rel="noopener noreferrer">{item.title}</a></td>
+                  <td>{item.user.login}</td>
+                  <td>{new Date(item.created_at).toLocaleDateString()}</td>
+                  <td>{item.comments}</td>
                 </tr>
-
-              {(index + 1) % 5 === 0 && (
-                <tr>
-                  <td colSpan="5">
-                    <a href="https://www.wanted.co.kr/" target="_blank" rel="noopener noreferrer">
-                      <img src={adImageURL} alt="Advertisement" />
-                    </a>
-                  </td>
-                </tr>
-              )}
-            </React.Fragment>
-          );
-              }
-        })}
-      </tbody>
-    </table>
+                );
+            } else {
+              return (
+                <React.Fragment key={item.id}>
+                  <tr>
+                  <td>{item.number}</td>
+                  <td><a href={`/issuelist/${item.number}`} target="_blank" rel="noopener noreferrer">{item.title}</a></td>
+                  <td>{item.user.login}</td>
+                  <td>{new Date(item.created_at).toLocaleDateString()}</td>
+                  <td>{item.comments}</td>
+                  </tr>
+                {(index + 1) % 5 === 0 && (
+                  <tr>
+                    <td colSpan="5">
+                      <a href="https://www.wanted.co.kr/" target="_blank" rel="noopener noreferrer">
+                        <img src={adImageURL} alt="Advertisement" />
+                      </a>
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            );
+                }
+          })}
+        </tbody>
+      </table>
+      </section>
+      <section className="background-footer">
+        <span></span>
+      </section>
+    </div>
   </div>
 
   );
